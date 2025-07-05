4c9c734d20cbc2d1194fbade239f3aa2d278ec2a
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-07-05 22:44:48 +0530

    backend start

---
+++ javascript/dataTypes/data.js
@@ -0,0 +1,22 @@
+// process.stdout.write("ashamin");
+// process.stdout.write("ashamin");
+
+/*
+DATA TYPES IN JAVASCRIPT
+1. Strings
+2. Number
+3. Boolean
+4. BigInt
+5. Undefined
+6. Null
+7. Objects
+8. Symbol
+*/
+
+let score = 20; //number
+let name = "ashamin"; //string
+let isLoggedin = false; // boolean
+let data = { name: "ashamin", age: 23, isMarred: false }; // Objects
+
+console.log(score);
+console.table(data); // print data for a tabular data
---
+++ javascript/operation/operation.js
@@ -0,0 +1,28 @@
+console.log("operation/operators in java script");
+// LOGICAL OPERATOR
+
+/*
+&& --> AND  operator
+in AND operation the every condition should be true be then its give (TRUE ) 
+other wise if a single condition false thens its return (FALSE)
+
+note: AND operator is found a first condition false the not check the other conditions 
+its direct returns false
+*/
+let isLoggedin = true;
+let isPaid = false;
+console.log(isLoggedin && isPaid);
+
+/*
+
+|| --> OR operator
+
+in OR operator if one condition is true then it's return true 
+if every condition is false then its return false
+
+*/
+
+let iAmAbove18 = true;
+let mySisterAbove18 = false;
+
+console.log(iAmAbove18 || mySisterAbove18);
