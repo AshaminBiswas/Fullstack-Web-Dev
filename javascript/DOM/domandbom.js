17401af932f46169f75c7b8dfdbde78c7b8956ee
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-07-11 17:42:10 +0530

    dom

---
+++ javascript/DOM/domandbom.js
@@ -0,0 +1,18 @@
+console.log("hello from script");
+// console.log(document.getElementsByTagName("h1"));
+
+/*
+access the element
+*/
+
+// const button = document.getElementById("button");
+// console.log(button);
+// button.addEventListener("click", () => {
+//   document.getElementsByTagName("h1");
+// });
+
+// const list = document.createElement("li");
+// list.textContent = "new list";
+// button.appendChild(list);
+
+document.getElementById("click").addEventListener()
