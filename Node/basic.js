78d8693bb6efec32daa32cedd99fa17a1432367d
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-07-24 22:30:40 +0530

    basic

---
+++ Node/basic.js
@@ -0,0 +1,22 @@
+let name = "ashamin";
+// console.log(typeof name);
+
+// Array
+
+let car = ["ashamin", "soumen", "robin"];
+car.push("tesla");
+
+// console.log(car[0]);
+
+for (let i = 1; i <= 10; i++) {
+  // console.log(i);
+}
+
+person = {
+  name: "ashamin",
+  roll: "frontend developer",
+  age: 23,
+  phone: 6297676908,
+};
+
+// console.log(person);
