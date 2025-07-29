638bf6f69cd208e43d5acb6a718630fe7d1dca91
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-07-29 18:32:03 +0530

    put and delete method in express

--- Node/resturent app/index.js
+++ Node/resturent app/index.js
@@ -2,61 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const db = require("./db.js");
-const person = require("./models/person.js");
-const menu = require("./models/menu.js");
 const bodyParser = require("body-parser");
-const menuItem = require("./models/menu.js");
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("This is Home rout");
+  res.send("This is Home route");
 });
 
-app.get("/person", async (req, res) => {
-  try {
-    const data = await person.find();
-    console.log("data fetched Successfully");
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+//import menu routes
+const menuRoutes = require("./routes/menu.route.js");
+// import person route
+const personRoutes = require("./routes/person.routes.js");
 
-app.get("/menu", async (req, res) => {
-  try {
-    const menuItems = await menu.find();
-    console.log("menuItemData Fetched Successfully");
-    res.status(200).json(menuItems);
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.post("/menu", async (req, res) => {
-  try {
-    const menus = req.body;
-    const menuItems = new menu(menus);
-    const menuItemData = await menuItems.save();
-    console.log("menuItemData is saved");
-    res.status(200).json(menuItemData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.post("/person", async (req, res) => {
-  try {
-    const data = req.body;
-    const personData = new person(data);
-    const savedPersonData = await personData.save();
-    console.log("data saved");
-    res.status(200).json(savedPersonData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "internal server error" });
-  }
-});
+//use menuRoute
+app.use("/", menuRoutes);
+//use person routes
+app.use("/", personRoutes);
 
 app.listen(port, () => {
   console.log(`The app is listening on port no : ${port} `);
---
+++ Node/resturent app/routes/menu.route.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const routes = express.Router();
+const menu = require("../models/menu.js");
+const menuItem = require("../models/menu.js");
+routes.get("/menu", async (req, res) => {
+  try {
+    const menuItems = await menu.find();
+    console.log("menuItemData Fetched Successfully");
+    res.status(200).json(menuItems);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+routes.get("/menu/:test", async (req, res) => {
+  try {
+    const testType = req.params.test;
+    if (testType === "sweet" || testType === "spicy" || testType === "sour") {
+      const response = await menu.find({ taste: testType });
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "Oops Not Found!" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+routes.post("/menu", async (req, res) => {
+  try {
+    const menus = req.body;
+    const menuItems = new menu(menus);
+    const menuItemData = await menuItems.save();
+    console.log("menuItemData is saved");
+    res.status(200).json(menuItemData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = routes;
