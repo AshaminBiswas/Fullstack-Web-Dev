8d5e55260353dda69f6acddba839969b5c2f00fb
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-07-30 18:10:00 +0530

    connect with mongo db Atlas

--- Node/resturent app/db.js
+++ Node/resturent app/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 //define connection url
-const dbURL = "mongodb://localhost:27017/hotels";
+// const dbURL = "mongodb://localhost:27017/hotels";
+
+const MONGO_PASSWORD = "ashamin1";
+const MONGO_URL = `mongodb+srv://ashamin_1:${MONGO_PASSWORD}@restapp.dowwhkt.mongodb.net/?retryWrites=true&w=majority&appName=RestApp`;
 
 //setup mongodb connect
-mongoose.connect(dbURL);
+mongoose.connect(MONGO_URL);
 
 const db = mongoose.connection;
 
