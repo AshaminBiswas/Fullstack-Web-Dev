649f48f5da149daa2d9e1e8a22addd6273afef4d
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-08-03 20:08:38 +0530

    use memo

--- react/useMemo/src/App.jsx
+++ react/useMemo/src/App.jsx
@@ -3,24 +3,31 @@ import './App.css'
 import Mycomponent from './components/Mycomponent'
 function App() {
   const [count, setCount] = useState(0)
-  const [input, setInput] = useState(0)
+  // const [input, setInput] = useState(0)
 
-  function doubleValue(num) {
-    for (let i = 1; i <= 1000000000; i++) { }
-    return num * 2
-  }
+  // function doubleValue(num) {
+  //   for (let i = 1; i <= 1000000000; i++) { }
+  //   return num * 2
+  // }
 
-  const memoizeValue = useMemo(() => {
-    return doubleValue(input)
-  }, [input])
+  // const memoizeValue = useMemo(() => {
+  //   return doubleValue(input)
+  // }, [input])
 
+  const num = new Array(3000000).fill(0).map((_, i) => {
+    return {
+      index: i,
+      isMagical: i === 2900000
+    }
+  })
+  const [number, setNumber] = useState(num)
 
-
+  const magical = useMemo(() => number.find((i) => i.isMagical === true), [number])
 
 
   return (
     <>
-      <Mycomponent count={count} setCount={setCount} />
+      {/* <Mycomponent count={count} setCount={setCount} />
 
       <h1>Use Memo</h1>
 
@@ -28,7 +35,12 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
       />
 
-      <p>Double value: {memoizeValue}</p>
+      <p>Double value: {memoizeValue}</p> */}
+
+      <p>{magical.index}</p>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+
     </>
   )
 }
