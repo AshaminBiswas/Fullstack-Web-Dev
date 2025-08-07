6abf5e47d60298c1519f47a6ec2216f2644063c3
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-08-07 10:16:04 +0530

    use Reducer with intermideat example

--- react/useReducer/src/App.jsx
+++ react/useReducer/src/App.jsx
@@ -7,18 +7,32 @@ import { useReducer } from 'react'
 function App() {
   // const [count, setCount] = useState(0)
    const reducer = (state, action) =>{
-      if(action.type === "Increment")return state + 1
-      if(action.type === "Decrement")return state - 1
+      switch (action.type) {
+        case "Increment":
+          return {count: state.count + 1}
+        case "Decrement":
+          return {count: state.count - 1}
+        case "Reset":
+          return {count : 0}
+        default :
+        return state
+      }
   }
-  const [count, dispatch] = useReducer(reducer, 0)
+
+  const initialState = {
+    count:0
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
  
 
   return (
     <>
-     <h1>Count is : {count}</h1>
+     <h1>Count is : {state.count}</h1>
      <button onClick={()=>dispatch({type:"Increment"})}>Increment</button>
-     <button onClick={()=>dispatch({type:"Decrement"})}>Increment</button>
+     <button onClick={()=>dispatch({type:"Decrement"})}>Decrement</button>
+     <button onClick={()=>dispatch({type:"Reset"})}>Reset</button>
     </>
   )
 }
