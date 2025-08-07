c46e14b482ac32755a9a13b616dccde9fe85a29e
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-08-07 10:33:19 +0530

    reset form using useReducer

--- react/useReducer/src/App.jsx
+++ react/useReducer/src/App.jsx
@@ -6,33 +6,76 @@ import { useReducer } from 'react'
 
 function App() {
   // const [count, setCount] = useState(0)
-   const reducer = (state, action) =>{
-      switch (action.type) {
-        case "Increment":
-          return {count: state.count + 1}
-        case "Decrement":
-          return {count: state.count - 1}
-        case "Reset":
-          return {count : 0}
-        default :
-        return state
-      }
-  }
+  //  const reducer = (state, action) =>{
+  //     switch (action.type) {
+  //       case "Increment":
+  //         return {count: state.count + 1}
+  //       case "Decrement":
+  //         return {count: state.count - 1}
+  //       case "Reset":
+  //         return {count : 0}
+  //       default :
+  //       return state
+  //     }
+  // }
 
   const initialState = {
-    count:0
+    name:"",
+    email:"",
+    password:""
   }
+  const reducer = (state, action)=>{
+    switch(action.type){
+      case "UPDATE_FORM":
+        return{
+          ...state,
+          [action.payload.field]:action.payload.value
+        }
+      case "RESET":
+        return initialState
 
+      default:
+        return state
+    }
+  }
   const [state, dispatch] = useReducer(reducer, initialState)
+    console.log(state)
+ const handleChange = (e)=>{
+    dispatch(
+      {
+        type:"UPDATE_FORM",
+        payload:{
+          field:e.target.name,
+          value:e.target.value
+        }
+      }
+    )
+ }
 
- 
-
+ const handleReset = ()=>{
+    dispatch(
+      {
+        type:"RESET"
+      }
+    )
+ }
   return (
     <>
-     <h1>Count is : {state.count}</h1>
+     {/* <h1>Count is : {state.count}</h1>
      <button onClick={()=>dispatch({type:"Increment"})}>Increment</button>
      <button onClick={()=>dispatch({type:"Decrement"})}>Decrement</button>
-     <button onClick={()=>dispatch({type:"Reset"})}>Reset</button>
+     <button onClick={()=>dispatch({type:"Reset"})}>Reset</button> */}
+
+     <input type="text" placeholder='enter name' name="name" value={state.name}
+      onChange={handleChange}
+     />
+     <input type="email" placeholder='enter email' name="email" value={state.email}
+      onChange={handleChange}
+     />
+     <input type="password" placeholder='enter password' name="password" value={state.password}
+      onChange={handleChange}
+     />
+     <button onClick={handleReset}>Reset</button>
     </>
   )
 }
