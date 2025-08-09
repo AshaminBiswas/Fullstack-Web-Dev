8ab8ecd489d5f2f52a3b67ed04ffca5ebcf1c9b3
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-08-09 19:49:57 +0530

    Refactor Chat class and update event handling for consistency

--- Node/events/eventClass.js
+++ Node/events/eventClass.js
@@ -3,17 +3,17 @@ const EventEmitter = require("events")
 
 
 // event driven architecture
-class Chat extends EventEmitter{
-    sendMessage(msg){
-        console.log("Message sent : ", msg);
-        this.emit("Message Received", msg)
-    }
-}
+// class Chat extends EventEmitter{
+//     sendMessage(msg){
+//         console.log("Message sent : ", msg);
+//         this.emit("Message Received", msg)
+//     }
+// }
 
-const chat = new Chat()
-chat.on("Message Received",(msg)=>{
-    console.log("New Message : ", msg);
-})
+// const chat = new Chat()
+// chat.on("Message Received",(msg)=>{
+//     console.log("New Message : ", msg);
+// })
 
 // chat.sendMessage("Hello Ashamin")
 
@@ -25,8 +25,37 @@ chat.on("Message Received",(msg)=>{
 
 //Error handling
 
-const eventEmitter = new EventEmitter()
-eventEmitter.on('error', (error)=>{
-    console.error(`Error Occurred : ${error.message}`)
+// const eventEmitter = new EventEmitter()
+// eventEmitter.on('error', (error)=>{
+//     console.error(`Error Occurred : ${error.message}`)
+// })
+// // eventEmitter.emit('error', new Error("Something went wrong"))
+
+
+
+
+class Chat extends EventEmitter{
+    sendMessage(message){
+        console.log(`Message Send : ${message}`)
+        this.emit("messageReceived", message)
+    }
+
+}
+
+const chatM = new Chat()
+chatM.on("messageReceived",(message)=>{
+    console.log(`New Message : ${message}`)
 })
-eventEmitter.emit('error', new Error("Something went wrong"))
+
+//trigger event
+chatM.sendMessage("Ashamin Biswas")
+
+
+
+const eventEmitter = new EventEmitter()
+
+eventEmitter.on('error',(err)=>{
+    console.error(`Error Occurred : ${err}`)
+})
+
+eventEmitter.emit('error', new Error("Something Went Wrong"))
